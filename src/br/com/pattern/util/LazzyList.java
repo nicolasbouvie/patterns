9d4b403af9b35f09957f38d899fe8de5303d08b3
Nícolas Bouvié da Silva <nicolasbouvie@gmail.com>
2014-11-28 11:25:36 -0200

    Alterado para extender de AbstractList, utilizando implementações default.

--- src/br/com/pattern/util/LazzyList.java
+++ src/br/com/pattern/util/LazzyList.java
@@ -2,7 +2,8 @@ package br.com.pattern.util;
 
 import java.io.File;
 import java.io.Serializable;
-import java.util.Collection;
+import java.util.AbstractList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.Map;
 
 import br.com.pattern.conn.Connection;
 
-public class LazzyList<E extends Serializable> implements List<E> {
+public class LazzyList<E extends Serializable> extends AbstractList<E> {
 	private final Map<Integer, E> values = new HashMap<>();
 	private final File[] files;
 	private final Connection conn;
@@ -26,16 +27,6 @@ public class LazzyList<E extends Serializable> implements List<E> {
 	@Override
 	public int size() {
 		return files.length;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return files.length == 0;
-	}
-
-	@Override
-	public boolean contains(Object o) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -63,56 +54,6 @@ public class LazzyList<E extends Serializable> implements List<E> {
 	}
 
 	@Override
-	public Object[] toArray() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public <T> T[] toArray(T[] a) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean add(E e) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean containsAll(Collection<?> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean addAll(Collection<? extends E> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean addAll(int index, Collection<? extends E> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void clear() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public E get(int index) {
 		if (values.size() <= index || values.get(index) == null) {
 			E el = conn.get(type, index);
@@ -123,42 +64,13 @@ public class LazzyList<E extends Serializable> implements List<E> {
 	}
 
 	@Override
-	public E set(int index, E element) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void add(int index, E element) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public E remove(int index) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public int indexOf(Object o) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public int lastIndexOf(Object o) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public ListIterator<E> listIterator() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public ListIterator<E> listIterator(int index) {
 		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public List<E> subList(int fromIndex, int toIndex) {
-		throw new UnsupportedOperationException();
+		File[] subFiles = Arrays.copyOfRange(files, fromIndex, toIndex);
+		return new LazzyList<E>(type, subFiles, conn);
 	}
 }
